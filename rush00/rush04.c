3f7eca226d35e48dd5c6450c246af09be68b5880
Syncx91 <nilsplsrd@gmail.com>
2025-09-16 14:18:33 +0200

    a

---
+++ rush00/rush04.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nplassar <nplassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 15:10:16 by nplassar          #+#    #+#             */
+/*   Updated: 2025/09/13 16:57:20 by nplassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_if(int a, int b, int x, int y);
+
+void	rush(int x, int y)
+{
+	int	a;
+	int	b;
+
+	if (x <= 0 || y <= 0)
+	{
+		write(1, "ERROR", 5);
+		return ;
+	}
+	b = 0;
+	while (b < y)
+	{
+		a = 0;
+		while (a < x)
+		{
+			ft_if(a, b, x, y);
+			a++;
+		}
+		ft_putchar('\n');
+		b++;
+	}
+}
+
+void	ft_if(int a, int b, int x, int y)
+{
+	if ((b == 0 && a == 0) || (b == y - 1 && a == x - 1 && y > 1 && x > 1))
+		ft_putchar('A');
+	else if ((b == 0 && a == x - 1) || (b == y - 1 && a == 0))
+		ft_putchar('C');
+	else if (b == 0 || b == y - 1 || a == 0 || a == x - 1)
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
